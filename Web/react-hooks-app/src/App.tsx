54d523972e959ec014111a4c3ae88b25487b4158
Raushan Kumar <sraushan890@gmail.com>
2026-04-06 03:54:14 +0530

    Refactor App component layout and styling for buttons

--- Web/react-hooks-app/src/App.tsx
+++ Web/react-hooks-app/src/App.tsx
@@ -1,5 +1,6 @@
 // import React from "react";
 import useCount from "./hooks/useCount";
+import "./App.css";
 
 function App() {
   const { count, incrementCount, decrementCount } = useCount();
@@ -7,9 +8,19 @@ function App() {
   return (
     <div>
       <h1>React Hooks</h1>
-      <button onClick={incrementCount}>+</button>
-      <h1>Count A Number : {count}</h1>
-      <button onClick={decrementCount}>-</button>
+      <div className="app">
+        <button className="btnPlus" onClick={incrementCount}>
+          +
+        </button>
+        <h1>Count A Number : {count}</h1>
+        <button
+          disabled={count === 0}
+          className="btnMinus"
+          onClick={decrementCount}
+        >
+          &minus;
+        </button>
+      </div>
     </div>
   );
 }
