7a4547e107a2119bf9bd9db529b25cf97fff8aca
Raushan Kumar <sraushan890@gmail.com>
2026-04-18 16:52:21 +0530

    Add Navigation and Logo components with styles
    - Implemented Navigation component with a sign-out button.
    - Created logo component using react-tilt for a dynamic effect.
    - Added corresponding CSS styles for both components to enhance UI.

--- face-recognition-brain/src/App.tsx
+++ face-recognition-brain/src/App.tsx
@@ -1,7 +1,19 @@
+import React, { Component } from "react";
 import "./App.css";
+import Navigation from "@/components/Navigation/Navigation";
+import Logo from "@/components/logo/Logo";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render(): React.ReactNode {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        {/* <ImageLinkForm/> */}
+        {/* <FaceRecognition/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
