dfda0e1e764932da82f3cd280ba85ddb1b1ae9b7
Raushan Kumar <sraushan890@gmail.com>
2026-04-22 00:07:46 +0530

    Remove commented-out code and add initial index.html file for static content

--- rest_api/server.js
+++ rest_api/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+// app.get("/", (req, res) => {
+//   console.log("====================================");
+//   console.log(req.headers, req.header);
+//   console.log("====================================");
+
+//   res.send("Hello, World!");
+// });
+
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000, () => {
   console.log(`Server is running on http://localhost:3000`);
