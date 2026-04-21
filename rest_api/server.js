5b07efdba25dd6318ba0c4c1be0a0b921b0f9a96
Raushan Kumar <sraushan890@gmail.com>
2026-04-21 15:55:32 +0530

    Fix incorrect import of express and streamline server port usage

--- rest_api/server.js
+++ rest_api/server.js
@@ -1,6 +1,5 @@
-const express = require("assert");
+const express = require("express");
 const app = express();
-const port = 3000;
 
 app.use(express.json());
 
@@ -8,6 +7,6 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`);
 });
