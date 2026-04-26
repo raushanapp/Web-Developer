888a79a40ae4f118f5b67c4bb89a430d36ed6d9f
Raushan Kumar <sraushan890@gmail.com>
2026-04-27 00:58:50 +0530

    Update bcryptjs dependency and enhance user authentication logic in server.js

--- smart_brain_api/server.js
+++ smart_brain_api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const knex = require("knex");
+const bcrypt = require("bcryptjs");
 
 const db = knex({
   client: "pg",
@@ -32,31 +33,62 @@ app.get("/", (req, res) => {
 
 app.post("/signin", (req, res) => {
   const { email, password } = req.body;
-  if (
-    // email === database.users[0].email &&
-    // password === database.users[0].password
-    true
-  ) {
-    res.json({ message: "Signin successful" });
-  } else {
-    res.status(401).json({ message: "Invalid credentials" });
-  }
+  db("login")
+    .select("email", "hash")
+    .from("login")
+    .where("email", "=", email)
+    .then((data) => {
+      let isValid = bcrypt.compareSync(password, data[0].hash); // true
+      if (isValid) {
+        return db("users")
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then((user) => {
+            res
+              .status(200)
+              .json({ message: "Signin successful", user: user[0] });
+          })
+          .catch(() => {
+            res.status(400).json({ message: "Unable to fetch user" });
+          });
+      } else {
+        res.status(400).json({ message: "Invalid credentials" });
+      }
+    })
+    .catch(() => {
+      res.status(400).json({ message: "Invalid credentials" });
+    });
 });
 
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
-  db("users")
-    .returning("*")
-    .insert({ name: name, email: email, joined: new Date() })
-    .then((user) => {
-      res.json({
-        message: "User registered successfully",
-        user: user[0],
-      });
-    })
-    .catch(() => {
-      res.status(400).json({ message: "Unable to register user" });
-    });
+  const hash = bcrypt.hashSync(password, 10);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then(async (loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({ name, email: loginEmail[0].email, joined: new Date() })
+          .then((user) => {
+            res.json({
+              message: "User registered successfully",
+              user: user[0],
+            });
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => {
+    console.error(err);
+    res.status(400).json({ message: "Unable to register user" });
+  });
 });
 
 app.get("/profile/:id", (req, res) => {
@@ -66,27 +98,29 @@ app.get("/profile/:id", (req, res) => {
     .from("users")
     .where({ id })
     .then((user) => {
-      res.json({ user });
+      if (user.length) {
+        res.json({ user });
+      } else {
+        res.status(400).json({ message: "User not found" });
+      }
     })
-    .catch(() => res.status(404).json({ message: "User not found" }));
+    .catch(() => res.status(400).json({ message: "User getting error" }));
 });
 
 app.put("/image", (req, res) => {
   let { id } = req.body;
-  let found = false;
-  db.users.forEach((user) => {
-    if (user.id === parseInt(id)) {
-      user.entries++;
-      found = true;
-      return res.json({
-        message: "Image updated successfully",
-        entries: user.entries,
-      });
-    }
-  });
-  if (!found) {
-    res.status(404).json({ message: "User not found" });
-  }
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res
+        .status(200)
+        .json({ message: "Image updated successfully", entries: entries[0] });
+    })
+    .catch(() => {
+      res.status(400).json({ message: "Error updating image" });
+    });
 });
 
 app.listen(3000, () => {
