9d30ca80a8762c7bc80c319b6a3e9000dffdbab7
Raushan Kumar <sraushan890@gmail.com>
2026-07-30 13:15:33 +0530

    Enhance counter functionality and add rerender capability; update App component to conditionally render based on counterName

--- Web/dom-manipulation/react-own/app.js
+++ Web/dom-manipulation/react-own/app.js
@@ -1,5 +1,6 @@
 const rootNode = document.getElementById("app");
 const root = ReactDOM.createRoot(rootNode);
+let counterName = "One";
 root.render(React.createElement(App));
 
 function App() {
@@ -10,18 +11,44 @@ function App() {
     React.createElement(
       "section",
       null,
-      React.createElement(Counter, null),
-      React.createElement(Counter, null),
+      counterName === "One"
+        ? React.createElement(Counter, { name: counterName })
+        : React.createElement(Counter2, { name: counterName }),
+      // React.createElement(Counter, { name: "two" }),
     ),
   );
 }
 
-function Counter() {
+// function Counter({ name }) {
+function Counter(props) {
+  //  here you can not modify props directly
+  props.name = "Three";
   return React.createElement(
     "article",
     null,
-    React.createElement("h2", null, "Counter"),
+    React.createElement("h2", null, "Counter", " ", props.name),
     React.createElement("p", null, "You clicked 1 times"),
     React.createElement("button", null, "Click me"),
   );
+}
+
+//  Template Logic
+
+function Counter2({ name }) {
+  //  you can modify local variables if you are not using the props directly
+  // name = "Three";
+
+  return React.createElement(
+    "article",
+    null,
+    React.createElement("h2", null, "Counter", " ", name),
+    React.createElement("p", null, "You clicked 1 times"),
+    React.createElement("button", null, "Click me"),
+  );
+}
+
+function rerender() {
+  console.log("Rerender...");
+  counterName = "Two";
+  root.render(React.createElement(App));
 }
