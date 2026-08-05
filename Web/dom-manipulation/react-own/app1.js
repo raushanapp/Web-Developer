1c34ac27601e4958fba920f28f4b382a16262320
Raushan Kumar <sraushan890@gmail.com>
2026-08-05 13:15:16 +0530

    Refactor App component to use Counter and Counter2 with dynamic naming; improve JSX structure for clarity

--- Web/dom-manipulation/react-own/app1.js
+++ Web/dom-manipulation/react-own/app1.js
@@ -1,20 +1,33 @@
 const rootNode = document.getElementById("app");
 const root = ReactDOM.createRoot(rootNode);
+const counterName = "One";
 root.render(React.createElement(App));
 
 function App() {
+  const counterOne = <Counter name={counterName} />;
+  const counterTwo = <Counter2 name={counterName} />;
   return (
-    <section>
+    <>
       <h1>Counters</h1>
-      <Counter />
-    </section>
+      {counterName === "One" ? counterOne : counterTwo}
+    </>
   );
 }
 
-function Counter() {
+function Counter({ name }) {
   return (
     <article>
-      <h2>Counter</h2>
+      <h2>Counter {name} </h2>
+      <p>You clicked 1 time</p>
+      <button className="button">Click me</button>
+    </article>
+  );
+}
+
+function Counter2({ name }) {
+  return (
+    <article>
+      <h2>Counter {name} </h2>
       <p>You clicked 1 time</p>
       <button className="button">Click me</button>
     </article>
