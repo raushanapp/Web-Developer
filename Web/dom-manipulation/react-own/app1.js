4973e0d4b63c7f00ed7ce608a6dedb268b1dd3e9
Raushan Kumar <sraushan890@gmail.com>
2026-08-05 14:54:29 +0530

    Refactor App component rendering; improve JSX structure and add linked list implementation

--- Web/dom-manipulation/react-own/app1.js
+++ Web/dom-manipulation/react-own/app1.js
@@ -1,7 +1,10 @@
 const rootNode = document.getElementById("app");
 const root = ReactDOM.createRoot(rootNode);
+
+console.log("===>>>", root);
 const counterName = "One";
-root.render(React.createElement(App));
+// root.render(React.createElement(App));
+root.render(<App />);
 
 function App() {
   const counterOne = <Counter name={counterName} />;
@@ -9,7 +12,7 @@ function App() {
   return (
     <>
       <h1>Counters</h1>
-      {counterName === "One" ? counterOne : counterTwo}
+      <section>{counterName === "One" ? counterOne : counterTwo}</section>
     </>
   );
 }
@@ -33,3 +36,56 @@ function Counter2({ name }) {
     </article>
   );
 }
+
+//  Rerender React
+
+function rerender() {
+  console.log("Rerendering...");
+  counterName = "Two";
+  root.render(<App />);
+}
+
+//  Linked list
+
+class LinkedListNode {
+  constructor(val, next = null) {
+    this.value = val;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(val) {
+    const newNode = new LinkedListNode(val);
+    //  if there is no head yet let's make new nodee a head.
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
+    }
+    //  Attach new node to the end of linked list.
+    this.tail.next = newNode;
+    this.tail = newNode;
+    return this;
+  }
+
+  print() {
+    let currentNode = this.head;
+    while (currentNode) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next;
+    }
+  }
+}
+
+let myLinkedList = new LinkedList();
+myLinkedList.append("Tony");
+myLinkedList.append("Mayur");
+myLinkedList.append("Alice");
+myLinkedList.append("Understanding React");
+myLinkedList.print();
