49c6f3cfb114857fa9e5a49bacfaed8de57a13fd
Raushan Kumar <sraushan890@gmail.com>
2026-08-14 08:45:25 +0530

    Refactor socket handling to use namespace for multiplayer functionality; improve room management and event broadcasting

--- Web/multiplayer-pong/sockets.js
+++ Web/multiplayer-pong/sockets.js
@@ -1,28 +1,38 @@
 let readyPlayerCount = 0;
+
 function listenForSocketEvents(io) {
-  io.on("connection", (socket) => {
+  const pongNamespace = io.of("/pong");
+
+  pongNamespace.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
+
+    let room;
+
     socket.on("ready", () => {
-      console.log("Player is ready:", socket.id);
+      room = "room" + Math.floor(readyPlayerCount / 2);
+      socket.join(room);
+
+      console.log("Player  ready:", socket.id, room);
       readyPlayerCount++;
       if (readyPlayerCount % 2 === 0) {
         //  broadcast to function to start game
-        io.emit("startGame", socket.id);
+        pongNamespace.in(room).emit("startGame", socket.id);
       }
     });
 
     socket.on("paddleMove", (paddleData) => {
       // Broadcast the paddle position to the other player
-      socket.broadcast.emit("paddleMove", paddleData);
+      socket.to(room).emit("paddleMove", paddleData);
     });
 
     socket.on("ballMove", (ballData) => {
       // Broadcast the ball position to the other player
-      socket.broadcast.emit("ballMove", ballData);
+      socket.to(room).emit("ballMove", ballData);
     });
 
     socket.on("disconnect", (reason) => {
       console.log(`Client ${socket.id} disconnected: ${reason}`);
+      socket.leave(room);
     });
   });
 }
