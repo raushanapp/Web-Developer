b224d379f0d45dbc1cc50874d858cd93150e21bf
Raushan Kumar <sraushan890@gmail.com>
2026-08-15 22:14:31 +0530

    Refactor Counter component; improve click handling logic and state management

--- Web/dom-manipulation/react-own/app2.js
+++ Web/dom-manipulation/react-own/app2.js
@@ -8,6 +8,7 @@ function App() {
       <section>
         <h1>Use Reducer</h1>
         <Counter name="First Counter" />
+        <Counter name="Second Counter" />
       </section>
     </>
   );
@@ -28,6 +29,27 @@ function Counter(props) {
   );
 
   const [numOfClicks, setNumOfClicks] = React.useState(0);
+  const [myName, setMyName] = React.useState("Tony");
+  //  this calling useState will be called on every render, so the value of myName will always be "Tony" on every render. The setMyName function will not be called, so the value of myName will never change. This is because the useState hook is called with the initial value of "Tony" on every render, and the setMyName function is never called to update the state.
+  //   let test = "";
+  //   let settest = null;
+  //   if (numOfClicks < 2) {
+  //     [test, settest] = React.useState("Tony 1");
+  //   }
+  const [myAge, dispatchMyAge] = React.useReducer(() => {}, { age: 30 });
+
+  // handle wrong click handler, this will not work as expected because the state is not updated immediately, so the value of numOfClicks will always be 0 when the function is called. The setNumOfClicks function is called three times, but the state is not updated until the next render, so the value of numOfClicks will always be 0 when the function is called.
+  function handleWrongClick() {
+    setNumOfClicks(numOfClicks + 1);
+    setNumOfClicks(numOfClicks + 1);
+    setNumOfClicks(numOfClicks + 1);
+  }
+  // handle correct click handler, this will work as expected because the state is updated immediately, so the value of numOfClicks will be 3 when the function is called. The setNumOfClicks function is called three times, and the state is updated immediately, so the value of numOfClicks will be 3 when the function is called.
+  function handleClick() {
+    setNumOfClicks((prevNumOfClicks) => prevNumOfClicks + 1);
+    setNumOfClicks((prevNumOfClicks) => prevNumOfClicks + 1);
+    setNumOfClicks((prevNumOfClicks) => prevNumOfClicks + 1);
+  }
 
   return (
     <article>
@@ -35,10 +57,13 @@ function Counter(props) {
       <p>You clicked {numOfClicks} times</p>
       <button
         className="btn btn-primary"
-        onClick={() => {
+        onClick={
+          //   () => {
           //   dispatch({ type: "Increment" });
-          setNumOfClicks(numOfClicks + 1);
-        }}
+          //   setNumOfClicks(numOfClicks + 1);
+          //   }
+          handleClick
+        }
       >
         Click me
       </button>
